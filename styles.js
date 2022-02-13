9fff2a2fc846dc0ddc054c9948feea59f9feba76
Erik V <eriksvdw@gmail.com>
2022-02-12 20:29:16 -0500

    Blocks made to only change when mouse is held down

--- styles.js
+++ styles.js
@@ -1,13 +1,13 @@
 // All blocks are square.
-// TODO: Compartmentalize further.
+
 
 // Constants
 var BLOCK_DIV = '<div class = "block"></div>';
-var TABLE_BLOCK = '<td class="block"></td>'
+var TABLE_BLOCK = '<td class="block"></td>';
 
-// Used by div method
+var mouseDown = false;
 
-function sectionAdd (reqBlocks) { // 
+function sectionAdd (reqBlocks) { // Used by div method
   var i = 0;
   while (i < reqBlocks) {
     $('div.block-container').append(BLOCK_DIV);
@@ -15,9 +15,7 @@ function sectionAdd (reqBlocks) { //
   }
 }
 
-// Used by table method
-
-function tableAdd (sideLength) {
+function tableAdd (sideLength) { // Used by table method
   var i = 0;
   
   while (i < sideLength) {
@@ -38,26 +36,51 @@ function tableAdd (sideLength) {
 
 // Used by both
 
+/* IDEA - NOT IN USE
+
+} */
+
 function changeColor () { // Changes the color on mouseover
-  $('.block').mouseover(function(){
-    $(this).css("background-color", "white");
+  // Add .click function for if it's already entered
+
+  $('.block').mouseenter(function(){
+    if (mouseDown) {
+      $(this).css("background-color", "white"); 
+    }
   });
 }
 
 function main(){
   
-  // Determine dimensions
+  // Determine dimensions / how many blocks to use
 
-  var sideLength = prompt("How many squares would you like per side on the grid?");
-  if (sideLength > 100) {sideLength = 100} // Set maximum for performance reasons (10,000 squares total)
+  var sideLength = parseInt(prompt("How many squares would you like per side on the grid?"),10); // Don't want any partial blocks
+
+  if (Number.isInteger(sideLength) == false || sideLength >100) {
+      console.log("Not an integer");
+      sideLength = 100 // Set maximum for performance reasons (10,000 squares total)
+  } 
+
   var dimension = (400/sideLength); // The width and height of each block
+
   var reqBlocks = Math.pow(sideLength, 2); // The number of blocks required to fill the area
   
-  // Divs
+  // Reseting and then using functions above to create a new grid
 
-  $('.block').remove();
+  // Div:
+
+  $('.block').remove(); 
   sectionAdd(reqBlocks);
-  $('.block').css({"width" : dimension, "height": dimension});
+  $('.block').css({"width" : dimension, "height": dimension}); // jQuery version
+  
+  /* var blocks = document.getElementsByClassName('block') // JS version
+    Array.from(blocks).forEach(function(block) {
+      block.style.width = dimension +"px";
+      block.style.height = dimension +"px";
+    });
+  */
+
+  // Table:
 
   $('tr').remove();
   tableAdd(sideLength);
@@ -66,6 +89,16 @@ function main(){
 }
 
 $(function(){
-  $('button').click(main);
-  changeColor();
+  
+  document.body.onmousedown = function() { 
+    mouseDown = true;
+  }
+  document.body.onmouseup = function() {
+    mouseDown = false;
+  }
+
+  $('button').click(main); // Call main function when "Clear" button clicked
+
+  changeColor();  // Change the color of blocks when entered
+
 });
